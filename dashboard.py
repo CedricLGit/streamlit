457664df8e18f80402cc6f36d0bc56aea0a22374
Cédric Louguet <cedric.louguet@gmail.com>
2021-12-07 12:01:46 +0100

    deploy

---
+++ dashboard.py
@@ -0,0 +1,349 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  4 14:27:29 2021
+
+@author: Cédric
+"""
+
+import pandas as pd
+import streamlit as st
+import numpy as np
+from functions import *
+import requests
+import pickle
+import matplotlib.pyplot as plt
+import re
+
+
+st.set_page_config(layout='wide')
+
+data = pd.read_csv('Data_streamlit/train_sample.csv')
+data = preprocess(data)
+data = cleaning(data)
+data = data.rename(columns = lambda x:re.sub('[^A-Za-z0-9_]+', '', x))
+sample = pd.read_csv('Data_streamlit/new_v3.csv')
+sample2 = sample.drop('TARGET', axis=1)
+st.set_option('deprecation.showPyplotGlobalUse', False)
+exp_expected_value, shap_values = explanation(sample2.drop('SK_ID_CURR', axis=1))
+model = pickle.load(open('selected_model_smote.sav', 'rb'))
+model['clf'].get_booster().feature_names = sample2.columns.tolist()
+shap_feat = pd.DataFrame(shap_values, columns=sample2.drop('SK_ID_CURR', axis=1).columns) 
+abs_shap_feat = abs(shap_feat).mean(axis=0).sort_values(ascending=False)
+top_feat = abs_shap_feat.index.values[:10]
+
+
+# Base Sidebar
+sb = st.sidebar
+
+col1, col2, col3 = sb.columns([1, 6, 1])
+
+with col2:
+    st.image('https://play-lh.googleusercontent.com/Q83pGT8fHMAx-Db_oaL0dHCY5-dB8nRLrwGolLeEAJSJjIqyfDr-mh8Q9AnnXHZgO8Y',
+             use_column_width=True)
+
+pages = sb.radio('', ('Home',
+                      'Analyse Exploratoire',
+                      'Explication du modèle',
+                      'Dashboard'))
+
+# Pages
+
+col4, col5, col6 = st.columns([1, 8, 1])
+c = st.container()
+
+if pages == 'Home':
+
+    with col5:
+        st.markdown('# <center>Application d\'analyse de risque</center>',
+                    True)
+
+    # with c:
+
+    st.write('')
+    st.markdown('### <div style="text-indent: 30px;"><u> Contexte</u></div>',
+                True)
+    st.write('')
+    st.write('''Cette application répond à la demande de la société financière
+             **"Prêt à dépenser"** de mettre en place un outil répondant aux 
+             problématiques suivantes :''')
+    st.markdown('* Calculer la probabilité qu\'un client rembourse son crédit.\
+                \n * Mettre en place un **dashboard interactif** permettant \
+                    d\'expliquer le choix d\'attribution d\'un crédit.')
+    st.write('')
+    st.markdown('### <div style="text-indent: 30px;"><u> Utilisation</u></div>',
+                True)
+    st.write('')
+    st.write('L\'application dispose de trois onglets :')
+    st.markdown('* **Analyse Exploratoire:** permettant de visualiser quelques\
+                informations sur le dataset.\
+                \n * **Explication du modèle :** reprenant les éléments\
+                    importants permettant d\expliquer celui-ci. \
+                \n * **Dashboard :** qui permettra d\'expliquer le choix \
+                    d\'attribution de crédit.')
+
+elif pages == 'Analyse Exploratoire':
+
+    with col5:
+        st.markdown('# <center>Analyse Exploratoire</center>', True)
+
+    st.write('')
+    st.write('')
+    st.write('')
+    st.markdown('### <div style="text-indent: 30px;"><u>Apperçu du jeu de données</u></div>',
+                True)
+    
+    col7, col8, col9 = st.columns([3.7,0.3,6])
+    with col7:
+        
+        st.write('')
+        st.write('')
+        st.write('')
+        st.markdown('<div style="text-align: justify;">Le jeu de données est \
+                    consitué de plusieurs fichiers articulés entre eux de la \
+                    manière ci-contre.\
+                    <BR>\
+                    <BR>Le fichier initial comprend les informations \
+                        principales concernant le prêt demandé ainsi que des \
+                        données sur le client. Par exemple nous retrouvons le \
+                        montant du prêt demandé et celui pour chaque échéances,\
+                        les revenus du client, son âge, etc ...\
+                    <BR>\
+                    <BR>Les autres fichiers contiennent des informations sur \
+                        les prêts et les demandes de prêt précédents. Ces \
+                        informations seront regroupées par client afin \
+                        d\'entrer en compte lors du calcul du risque de défaut\
+                        de remboursement du crédit.</div>',
+                    True)
+    with col9:
+        st.image('https://storage.googleapis.com/kaggle-media/competitions/home-credit/home_credit.png',
+                 use_column_width=True)
+    st.write('')
+    st.write('')
+    shape = data.shape
+    st.markdown('Voici un apperçu du fichier principal : application_train.\
+                Celui-ci a une entrée par client et contient:\
+                <BR>\
+                <BR>\
+                <li> '+ str(shape[0]) + ' lignes/clients</li>\
+                <li> '+ str(shape[1]) + ' colonnes/informations sur celui-ci</li>',
+                True)
+    st.write('')
+    
+    st.dataframe(data.head(8))
+    st.write('')
+    st.write('')
+    st.markdown('### <div style="text-indent: 30px;"><u>Déséquilibre des classes</u></div>',
+                True)
+
+    col10, col11 = st.columns(2)
+    with col10:
+        st.plotly_chart(countplot(data, 'TARGET'), True)
+    with col11:
+        st.write('')
+        st.write('')
+        st.write('')
+        st.write('')
+        st.write('')
+        st.markdown('<div style="text-align: justify;">De par la nature du \
+                    problème étudié (risque de défaut de remboursement), nous\
+                    pouvons constater un fort déséquilibre des classes.\
+                    <BR>La classe 1 correspondant aux clients qui n\'nont pas\
+                        rencontré de défaut de paiement représente environ 90%\
+                        de nos clients.\
+                    <BR>\
+                    <BR>Pour de prendre en compte cet effet et pour prendre en\
+                        compte l\'aspect métier (à savoir prioriser la\
+                        détection du risque de défaut de paiement), la mise\
+                        en place d\'une métrique personnalisée sera nécessaire.</div>',
+                    True)
+        
+
+    st.markdown('### <div style="text-indent: 30px;"><u>Corrélation linéaire</u></div>',
+                True)
+    st.write('')
+    st.markdown('Afin d\'avoir une première idée des variables/informations\
+                importantes lors du choix d\'attribution d\'un crédit, nous\
+                pouvons regarder les variables qui sont le plus corrélées avec\
+                la classe. En voici ci-dessous la liste.')
+    st.write('')
+    st.table(correlation(data))
+    st.write('')
+    st.markdown('Nous pouvons regarder plus en détail leur distribution respective\
+                en prenant en compte la classe des individus afin d\'identifier\
+                un impact potentiel sur l\'attribution d\'un prêt.')
+    
+    col12, col13 = st.columns([9,1])
+    with col12:    
+        choices = st.multiselect(label ='Choisir les variables à afficher',
+                                 options=correlation(data).index)
+    with col13:
+        st.write('')
+        st.write('')
+        button = st.button('Show me')
+
+    if button:
+
+        if len(choices) != 0:
+            st.plotly_chart(distribution(data, choices), True)
+        else:
+            st.markdown('Veuillez sélectionner des variables à étudier.')
+
+elif pages == 'Explication du modèle':
+
+    with col5:
+        st.markdown('# <center>Explication du modèle</center>', True)
+    st.write('')
+    st.markdown('### <div style="text-indent: 30px;"><u>Fonction coût métier</u></div>',
+                True)
+    st.write('')
+    col7, col8 = st.columns(2)
+    with col7:  
+        st.markdown('<div style="text-align: justify;">La problématique à \
+                    laquelle le modèle cherche à répondre est la prédiction \
+                    d\'un risque de défault de paiement de la part d\'un client.\
+                    Pour celà nous allons mettre en place un fonction customisée\
+                    qui comparera la prédiction de notre modèle et la classe \
+                    réelle de nos individus en pondérant les erreurs observées.\
+                    Nous attribuerons un coefficient plus important aux erreurs\
+                    lorsque le modèle se trompera sur un client qui a rencontré\
+                    des difficultées de paiement sur le jeu d\'entraînement.\
+                    <BR>\
+                    <BR>Par ailleurs nous utiliserons cette fonction afin d\'optimiser\
+                        le treshold de prédiction. Ceci permettra d\'obtenir de \
+                        meilleurs résultats comment le montrent les matrices de\
+                        confusions ci contre.<div>',
+                    True)
+    st.write('')
+    st.write('')
+    st.markdown('### <div style="text-indent: 30px;"><u>Analyse du modèle</u></div>',
+                True)
+    st.write('')
+    col9, col10 = st.columns([0.4, 0.6])
+    with col9:
+        st.markdown('<div style="text-align: justify;">Après avoir entraîné le \
+                    modèle, nous pouvons regarder quelles features contribuent \
+                    le plus aux prédictions de celui ci.\
+                    <BR>\
+                    <BR>Dans un premier temps, nous pouvons récupérer les features\
+                    importances que nous renvoie le modèle selectionné.\
+                    <BR>XGBoost nous proposent deux types de feature importance:\
+                        weight qui représente le nombre de fois qu\'une feature\
+                        apparaît dans un arbre de décision ainsi que le gain qui\
+                        lui représente la moyenne de la réduction de la fonction\
+                        coût quand la feature est utilisée au niveau d’un noeud.</div>',
+                    True)
+        st.write('')            
+        choices = st.selectbox(label ='Choisir le type de feature importance',
+                               options=['weight', 'gain'],
+                               index=1)
+    with col10:
+        xgb.plot_importance(model['clf'],
+                            max_num_features=10,
+                            importance_type=choices,
+                            xlabel='Feature importance value')
+        st.pyplot(bbox_inches='tight')
+        plt.clf()
+    st.write('')
+    st.write('')
+    col11, col12 = st.columns([0.6, 0.4])
+    with col12:
+        st.write('')  
+        st.markdown('<div style="text-align: justify;">Nous pouvons ensuite nous\
+                    tourner vers SHAP pour expliquer notre modèle.\
+                    <BR>En moyennant les valeurs absolues des valeurs de Shap\
+                        pour chaque feature, nous pouvons remonter à l\'importance\
+                        globale des variables.\
+                    <BR>Nous pouvons aussi représenter les valeurs de Shap pour\
+                        chaque individu.</div>',
+                    True)
+        st.write('')  
+        choices2 = st.selectbox(label ='Choisir le type de feature importance',
+                               options=['dot', 'bar'],
+                               index=1)
+    with col11:
+        shap.summary_plot(shap_values,
+                          sample2.drop('SK_ID_CURR', axis=1),
+                          max_display=10,
+                          plot_type=choices2)
+        st.pyplot(bbox_inches='tight')
+        plt.clf()
+
+elif pages == 'Dashboard':
+
+    with col5:
+        st.markdown('# <center>Dashboard</center>', True)
+
+    with c:
+        col7, col8, col9 = st.columns(3)
+
+        with col8:
+            st.write("")
+            client_id = st.selectbox('Client selection',
+                                      options=sample.SK_ID_CURR.sort_values().values)
+
+            data_input = {"input": sample2[sample2.SK_ID_CURR==int(client_id)].drop('SK_ID_CURR', axis=1).values.tolist()}
+            headers = {"Content-Type": "application/json"}
+            result = requests.get('https://oc-p7-flaskapi.herokuapp.com/predict',
+                                  headers=headers,
+                                  json=data_input).text
+            st.markdown('### <center>SCORE = '+ result+'</center>',
+                        True)        
+                
+            if float(result)>0.22346503:
+                st.markdown('<center>Le score est supérieur au threshold fixé.\
+                            <BR>Le crédit est <b>refusé</b>.</center>',
+                            True)
+            else:
+                st.markdown('<center>Le score est inférieur au threshold fixé.\
+                            <BR>Le crédit est <b>accepté</b>.</center>',
+                            True)
+        
+    
+    st.markdown('### <div style="text-indent: 30px;"><u>Données du client</u></div>',
+                True)
+    st.write('')    
+    st.dataframe(data[data.SK_ID_CURR==client_id].drop('TARGET', axis=1))
+    st.write('')
+    
+    st.markdown('### <div style="text-indent: 30px;"><u>Explication de la\
+                décision</u></div>',
+                True)
+    st.write('')
+    st.markdown('Afin d\'expliquer l\'attribution du crédit, nous pouvons \
+                regarder l\'impact local des variables sur la valeur Shap\
+                du client sélectionné. Nous pouvons voir les variables qui\
+                impactent le plus la prédiction du modèle pour celui ci.\
+                <BR>En rouge nous avons les variables qui contribuent à ce \
+                    que la prédiction soit plus élevée que la valeur de base,\
+                    et en bleu celles qui ont l\'effet inverse.',
+                True)
+    idx = sample2[sample2.SK_ID_CURR==int(client_id)].index
+    
+    shap.force_plot(exp_expected_value, shap_values[idx,:],
+                    sample2.drop('SK_ID_CURR', axis=1).columns.tolist(),
+                    contribution_threshold=0.025,
+                    matplotlib=True)
+    st.pyplot(bbox_inches='tight')
+    plt.clf()
+    st.write('')
+    
+    st.markdown('### <div style="text-indent: 30px;"><u>Comparaison</u></div>',
+                True)
+    ref = data.drop(['SK_ID_CURR', 'TARGET'], axis=1)
+    commun = shap_feat.copy()
+    commun, ref = commun.align(ref, join = 'inner', axis=1) 
+            
+    values_client = commun.loc[idx]
+
+    top10_pos = values_client.sort_values(idx[0], ascending=False,
+                                          axis=1)
+    top10_pos = top10_pos.columns[:10]
+
+    choice = st.multiselect('Choisir les variables à comparer',
+                            options=top10_pos)                                 
+    if len(choice) != 0:
+        st.plotly_chart(distribution(sample,
+                                      choice,
+                                      comparaison=True,
+                                      id_client=int(client_id)),
+                        use_container_width=True)
---
+++ functions.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 15 17:14:55 2021
+
+@author: Cédric
+"""
+
+import numpy as np
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+from plotly.subplots import make_subplots
+from sklearn.preprocessing import LabelEncoder
+import pickle
+import shap
+import matplotlib.pyplot as plt
+import xgboost as xgb
+
+
+@st.cache
+def preprocess(df):
+    
+    '''
+    Encoder les variables qualitatives
+    '''    
+    
+    # Encoder en 0/1 les categories avec seulement 2 valeurs
+    le = LabelEncoder()
+    
+    for col, col_type in df.dtypes.iteritems():
+        if (col_type == 'object') and (len(df[col].unique()) == 2):
+            le.fit(df[col])
+            le.transform(df[col])
+            
+    # Encoder en 'OneHot' les variables de type category
+    df = pd.get_dummies(df)
+
+    # Il y a des valeurs anormales pour 'DAYS_EMPLOYED' que l'on va supprimer  
+    df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace=True)    
+    
+    return df
+
+@st.cache # Cache pour performances streamlit
+def cleaning(df):
+    
+    # Supprimer les colonnes qui ont plus de 50% de na
+    
+    perc = 0.5*df.shape[0]
+    
+    df_clean = df.dropna(axis=1, thresh=perc).copy()
+    
+    # Imputer les nans
+    
+    numdata = df_clean.select_dtypes('number')
+    datcol = df_clean.select_dtypes('number')
+    
+    for col in numdata.columns[numdata.isna().any()]:
+        df_clean[col].fillna(df_clean[col].mean(), inplace = True)
+    
+    return df_clean
+
+@st.cache
+def countplot(dataframe, x):
+    
+    dtype = dataframe[x].dtype
+    dataframe[x] = dataframe[x].astype(str) # Convertir en string pour une meilleure visualisation
+
+    fig = px.histogram(dataframe,
+                       x=x,
+                       color='TARGET')
+    fig.update_layout(yaxis_title='',
+                      title={'text': 'Nombre de clients par classe',
+                             'x': 0.5,
+                             'y': 0.03,
+                             'xanchor': 'center',
+                             'yanchor': 'bottom'})
+    
+    dataframe[x] = dataframe[x].astype(dtype) # Reconvertir au format d'origine
+    
+    return fig
+
+@st.cache
+def correlation(dataframe):
+    
+    corr = dataframe.corr()
+    top10 = abs(corr.TARGET.drop('TARGET')).sort_values(ascending=False).head(10).index
+    top10_corr = corr.TARGET.drop('TARGET')[top10].sort_values(ascending=False)
+    
+    return top10_corr
+
+@st.cache
+def distribution(dataframe, features, comparaison=False, id_client=''):
+    
+    nb_row = (len(features)//2)+1
+    
+    fig = make_subplots(rows=nb_row, cols=2,
+                        subplot_titles=['{}'.format(feat) for feat in features],
+                        vertical_spacing=0.05)
+    
+    for i,feat in enumerate(features):
+        
+        if i%2 == 0:
+            col=1
+        else:
+            col=2           
+        
+        data0 = dataframe[dataframe.TARGET==0][feat]
+        data1 = dataframe[dataframe.TARGET==1][feat]
+        
+        fig2 = ff.create_distplot([data0, data1],
+                                  ['Prêt sans défault de paiement', 'Prêt avec défault de paiement'],
+                                  show_hist=False, show_rug=False)
+        
+        
+        if i !=0:
+            fig2['data'][0]['showlegend'] = False
+            fig2['data'][1]['showlegend'] = False
+        
+        
+        fig.add_trace(fig2['data'][0],
+                      row=(i//2)+1, col=col)
+        fig.add_trace(fig2['data'][1],
+                      row=(i//2)+1, col=col)
+        
+        if comparaison == True:
+            
+            fig.add_vline(x=dataframe[dataframe.SK_ID_CURR == id_client][feat].values[0],
+                          line_dash="dash",
+                          annotation_text="client value",
+                          annotation_font_size=15,
+                          row=(i//2)+1, col=col)
+        
+    fig.update_layout(height=500*nb_row)
+    
+    return fig
+
+@st.cache
+def explanation(df_train):
+    
+    model = pickle.load(open('model_smote_v3.sav', 'rb'))
+    explainer = shap.TreeExplainer(model['clf'])
+    shap_values = explainer.shap_values(df_train)
+
+    return explainer.expected_value, shap_values
+
+@st.cache
+def feature_imp(type_feat):
+    
+    model = pickle.load(open('model_smote_v3.sav', 'rb'))
+    
+    if type_feat == 'xgb':
+        
+        fig = xgb.plot_importance(model['clf'],
+                                  max_num_features=10,
+                                  importance_type='gain')
+    
+    return fig
